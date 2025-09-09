04cf54a4f5ceb4e42fbe0c13a19311a2607842f4
ABHAY PRATAP SINGH <itsabhaypratapsingh03@gmail.com>
2025-09-09 18:41:45 +0530

    Java Regex and Junit

---
+++ JavaRegexAndJunit/JavaJunit/src/main/java/com/junit/examples/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package JavaRegexAndJunit.JavaJunit.src.main.java.com.junit.examples;
+
+public class DatabaseConnection {
+    private boolean connected = false;
+    
+    public void connect() {
+        System.out.println("Connecting to database...");
+        connected = true;
+    }
+    
+    public void disconnect() {
+        System.out.println("Disconnecting from database...");
+        connected = false;
+    }
+    
+    public boolean isConnected() {
+        return connected;
+    }
+}
